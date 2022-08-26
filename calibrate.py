9a9ba3dac913f073a22598c5e8c98f0544b982cc
Angel Carro-Lagoa <angel.carro@udc.es>
2022-08-26 10:32:11 +0200

    Added several program options to calibrate.py and undistort.py
    Checkerboard pattern size, calibration termination criteria (iterations and precision), and other calibration options are now configurable in calibrate.py.
    A --save_dets_dir argument is also provided to export the images with detected pattern to a directory, allowing to manually discard blurry images.
    
    undistort.py now uses cv2.getOptimalNewCameraMatrix() with a free scaling parameter (alpha) to control the quantity of original image pixels discarded in the output.

--- calibrate.py
+++ calibrate.py
@@ -9,15 +9,27 @@ import numpy as np
 import yaml
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Calibrate camera using a video of a chessboard or a sequence of images.')
+    parser = argparse.ArgumentParser(description='Calibrate camera using a video of a chessboard or a sequence of images.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('input', help='input video file or glob mask')
     parser.add_argument('out', help='output calibration yaml file')
     parser.add_argument('--debug-dir', help='path to directory where images with detected chessboard will be written',
                         default=None)
     parser.add_argument('-c', '--corners', help='output corners file', default=None)
-    parser.add_argument('-fs', '--framestep', help='use every nth frame in the video', default=20, type=int)
+    parser.add_argument('-fs', '--framestep', help='use every nth frame in the video', default=1, type=int)
     parser.add_argument('-max', '--max-frames', help='limit the number of frames used for calibration', default=None, type=int)
     # parser.add_argument('--figure', help='saved visualization name', default=None)
+    parser.add_argument('-ps', '--pattern_size', help='The pattern size', default=[9, 6], type=int, nargs=2)
+    parser.add_argument('--save_dets_dir', help='path to directory where images with detected chessboard will be written',
+                        default=None)
+    parser.add_argument('--term_iters', help='Calibration termination criteria iterations', default=30, type=int)
+    parser.add_argument('--term_eps', help='Calibration termination criteria epsilon', default=np.finfo(float).eps, type=float)
+    parser.add_argument('--fix_principal_point',
+                        help='The principal point is not changed during the global optimization. It stays at the center',
+                        default=False, action='store_true')
+    parser.add_argument('--zero_tangent_dist',
+                        help='Tangential distortion coefficients (p1,p2) are set to zeros and stay zero.',
+                        default=False, action='store_true')
     args = parser.parse_args()
 
     if '*' in args.input:
@@ -26,7 +38,7 @@ if __name__ == '__main__':
         source = cv2.VideoCapture(args.input)
     # square_size = float(args.get('--square_size', 1.0))
 
-    pattern_size = (9, 6)
+    pattern_size = tuple(args.pattern_size)
     pattern_points = np.zeros((np.prod(pattern_size), 3), np.float32)
     pattern_points[:, :2] = np.indices(pattern_size).T.reshape(-1, 2)
     # pattern_points *= square_size
@@ -40,8 +52,10 @@ if __name__ == '__main__':
         frame += 1
         if isinstance(source, list):
             # glob
-            if frame == len(source):
+            if frame >= len(source):
                 break
+            if frame % args.framestep != 0:
+                continue
             img = cv2.imread(source[frame])
         else:
             # cv2.VideoCapture
@@ -54,7 +68,8 @@ if __name__ == '__main__':
         print(f'Searching for chessboard in frame {frame}... ', end='')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         h, w = img.shape[:2]
-        found, corners = cv2.findChessboardCorners(img, pattern_size, flags=cv2.CALIB_CB_FILTER_QUADS)
+        fcc_flags = cv2.CALIB_CB_FILTER_QUADS # + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE
+        found, corners = cv2.findChessboardCorners(img, pattern_size, flags=fcc_flags)
         if found:
             term = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1)
             cv2.cornerSubPix(img, corners, (5, 5), (-1, -1), term)
@@ -71,7 +86,10 @@ if __name__ == '__main__':
         if args.debug_dir:
             img_chess = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
             cv2.drawChessboardCorners(img_chess, pattern_size, corners, found)
-            cv2.imwrite(os.path.join(args.debug_dir, '%04d.png' % frame), img_chess)
+            cv2.imwrite(os.path.join(args.debug_dir, f'{frame:04d}{["_NF",""][found]}.png'), img_chess)
+        if args.save_dets_dir and found:
+            img_chess = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            cv2.imwrite(os.path.join(args.save_dets_dir, f'{frame:04d}.png'), img_chess)
 
     if args.corners:
         with open(args.corners, 'wb') as fw:
@@ -86,7 +104,11 @@ if __name__ == '__main__':
 #        w, h = pickle.load(fr)
 
     print('\ncalibrating...')
-    rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (w, h), None, None)
+    calib_flags = cv2.CALIB_FIX_PRINCIPAL_POINT * args.fix_principal_point
+    calib_flags += cv2.CALIB_ZERO_TANGENT_DIST * args.zero_tangent_dist
+    term_crit = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, args.term_iters, args.term_eps)
+    rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, (w, h), None, None,
+                                                                       flags=calib_flags, criteria=term_crit)
     print("RMS:", rms)
     print("camera matrix:\n", camera_matrix)
     print("distortion coefficients: ", dist_coefs.ravel())
--- undistort.py
+++ undistort.py
@@ -12,11 +12,23 @@ if __name__ == '__main__':
     parser.add_argument('calibration', help='input video file')
     parser.add_argument('input_mask', help='input mask')
     parser.add_argument('out', help='output directory')
+    parser.add_argument('--crop_roi', action='store_true', default=False)
+    parser.add_argument('--alpha', default=0.0, type=float)
     args = parser.parse_args()
 
     with open(args.calibration) as fr:
         c = yaml.safe_load(fr)
+        mtx = np.array(c['camera_matrix'])
+        dist = np.array(c['dist_coefs'])
 
+    print(f"Camera matrix:\n{mtx}")
+    print(f"dist:{dist}")
+
+    if not os.path.exists(args.out):
+        print(f"Creating output directory as it does not exist: {args.out}")
+        os.makedirs(args.out)
+
+    oldhw = None
     for fn in glob(args.input_mask):
         print(f'processing {fn}...')
         img = cv2.imread(fn)
@@ -24,13 +36,27 @@ if __name__ == '__main__':
             print(f"failed to load {fn}")
             continue
 
-        K_undistort = np.array(c['camera_matrix'])
-        # K_undistort[0:2, 2] = [0., 0.]
-        # K_undistort[0, 0] *= 0.3
-        # K_undistort[1, 1] *= 0.3
-        img_und = cv2.undistort(img, np.array(c['camera_matrix']), np.array(c['dist_coefs']),
-                                newCameraMatrix=K_undistort)
-        name, ext = os.path.splitext(os.path.basename(fn))
-        cv2.imwrite(os.path.join(args.out, name + '_und' + ext), img_und)
+        h,  w = img.shape[:2]
+        if oldhw != (h,w):
+            newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), alpha=args.alpha, newImgSize=(w, h))
+            print(f"WxH: {w}x{h}")
+            print(f"New camera matrix:\n{newcameramtx}")
+            print(f"ROI:{roi}")
+            oldhw = h,w
 
-        print('ok')
+        img_und = cv2.undistort(img, mtx, dist,
+                                newCameraMatrix=newcameramtx)
+
+        if args.crop_roi:
+            # crop the image
+            x, y, w, h = roi
+            img_und = img_und[y:y+h, x:x+w]
+
+        name, ext = os.path.splitext(os.path.basename(fn))
+        name += '_und' + ['', '_crop'][args.crop_roi] + ['', f'_{args.alpha}'][args.alpha != 0.0]
+        out_fname = os.path.join(args.out, name + ext)
+        write_ok = cv2.imwrite(out_fname, img_und)
+
+        if not write_ok:
+            print(f"Error writing file: {out_fname}")
+            exit(-1)
